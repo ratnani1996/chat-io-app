1c01b15b5768a5f03cd0b7e9ae3530641c377151
ratnani1996 <himanshuratnani1996@gmail.com>
2017-07-21 00:29:17 +0530

    added broadcasting partially part 1

--- public/JS/index.js
+++ public/JS/index.js
@@ -2,9 +2,7 @@ var socket = io();
 
 socket.on('connect', ()=>{
     console.log(`Connection to the server is up and running`)
-
-    socket.emit('createMessage', "This is a new message");
-
+    
     socket.on('newMessage', (message)=>{
         console.log(message);
     })
--- server/socket-app.js
+++ server/socket-app.js
@@ -3,10 +3,9 @@ function iofunctionality(io){
     io.on('connection', (socket)=>{
         console.log(`User connected`);
 
-        socket.emit('newMessage', "This is a new message");
-
         socket.on('createMessage', (message)=>{
             console.log(message);
+            io.emit('newMessage', message);
         })
 
         socket.on('disconnect', ()=>{console.log(`User disconnected`)});
