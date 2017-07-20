3d0e2295ec8267fa961a67c65008e8042e182fa1
ratnani1996 <himanshuratnani1996@gmail.com>
2017-07-21 00:50:18 +0530

    added socket.broadcast.emit on connection and disconnection

--- server/socket-app.js
+++ server/socket-app.js
@@ -3,12 +3,19 @@ function iofunctionality(io){
     io.on('connection', (socket)=>{
         console.log(`User connected`);
 
+        socket.emit('newMessage', "WELCOME to the chat app from ADMIN");
+        socket.broadcast.emit('newMessage', "New user has joined the room");
+
         socket.on('createMessage', (message)=>{
             console.log(message);
-            io.emit('newMessage', message);
+            // io.emit('newMessage', message);
+            // socket.broadcast.emit('newMessage', {message});
         })
 
-        socket.on('disconnect', ()=>{console.log(`User disconnected`)});
+        socket.on('disconnect', ()=>{
+            console.log(`User disconnected`)
+            socket.broadcast.emit('newMessage', "One user has diconnected");
+        });
     })
 }
 
